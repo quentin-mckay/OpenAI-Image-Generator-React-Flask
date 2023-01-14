8b157edb7c49f3594850d78440749060e92ade77
Quentin McKay <quentinmckay1@gmail.com>
2023-01-14 11:34:10 +1100

    generateImage json send and returned successfully

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,24 +1,51 @@
-from flask import Flask
+from flask import Flask, request
 import openai
+import random
 
 app = Flask(__name__)
 
-OPENAI_API_KEY = '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'
+OPENAI_API_KEY = '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'
 
 openai.api_key = OPENAI_API_KEY
+
 
 @app.route('/')
 def index(): 
     return 'hello'
 
 
+
+# return static json
 @app.route('/test')
 def test():
     return {'apples': 1, 'bananas': 2, 'cherries': 3}
 
 
-@app.route('/generateimage')
+
+# return different json each time
+@app.route('/randomword')
+def randomword():
+    word = random.choice(['apples', 'bananas', 'cherries'])
+    return {'word': word}
+
+
+
+@app.route('/posttest', methods=['POST'])
+def posttest():
+    prompt = request.json['prompt']
+    size = request.json['size']
+    print(prompt, size)
+    return 'got posttest'
+
+
+
+
+
+@app.route('/generateimage', methods=['POST'])
 def generate_image():
+
+    print(request.json)
+
     try:
         response = openai.Image.create(
             prompt='snowflake wearing a hat',
@@ -32,6 +59,8 @@ def generate_image():
             "success": True,
             "image_url": image_url
         }
+
+        
 
     except openai.error.OpenAIError as e:
         print(e.http_status)
--- react-client/src/App.js
+++ react-client/src/App.js
@@ -2,33 +2,102 @@ import { useState, useEffect } from 'react'
 
 export default function App() {
 
-	const [data, setData] = useState({})
+	const [data, setData] = useState('')
+
+	const [prompt, setPrompt] = useState('snowflake wearing a hat')
+	const [size, setSize] = useState('512x512')
+
 	const [imageUrl, setImageUrl] = useState({})
 
-	const fruits = Object.entries(data).map(([key, value]) => {
-		return <p>{key} {value}</p>
-	})
+
+	// iterate over object, returns an array of arrays [[key, value], [key, value]]
+	// const fruits = Object.entries(data).map(([key, value]) => {
+	// 	return <p>{key} {value}</p>
+	// })
 
 	useEffect(() => {
-		fetch('/test')
+		
+
+		
+	}, [])
+
+	function handleSubmit(event) {
+		event.preventDefault()
+	}
+
+	function fetchtest() {
+		console.log(prompt)
+
+		fetch('/randomword')
 			.then(res => res.json())
 			.then(data => {
 				setData(data)
 				console.log(data)
 			})
+	}
 
-		fetch('/generateimage')
-			.then(res => res.json())
-			.then(data => {
-				setImageUrl(data.image_url)
-				console.log(data)
+	// sending post request with json body
+	// as an alternative to using form action and form data
+	// not sure which one is better (perhaps this way is more modern?)
+	function posttest() {
+		console.log('sent post request')
+		fetch('/posttest', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				prompt,
+				size
 			})
-	}, [])
+		})
+	}
 	
+	function generateImage() {
+		fetch('/generateimage', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				prompt,
+				size
+			})
+		})
+		.then(res => res.json())
+		.then(data => {
+			console.log(data)
+			setData(data)
+		})
+	}
+	// fetch('/generateimage')
+		// 	.then(res => res.json())
+		// 	.then(data => {
+		// 		setImageUrl(data.image_url)
+		// 		console.log(data)
+		// 	})
+
 	return (
 		<main>
 			<h1>Hello there</h1>
-			{fruits}
+
+			<form action="" onSubmit={handleSubmit}>
+				<input 
+					type="text" 
+					placeholder='Prompt'
+					autoFocus
+					name="prompt"
+					value={prompt}
+					onChange={e => setPrompt(e.target.value)}
+				/>
+				
+				<button onClick={posttest}>Post Test</button>
+				<button onClick={generateImage}>Generate Image</button>
+			</form>
+
+			{data.word}
+
+			<img src={data.image_url} alt="" />
 			
 		</main>
 	)
