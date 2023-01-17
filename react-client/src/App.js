bcfcb297e010508e54bd98b4f4211aba404018fc
Quentin McKay <quentinmckay1@gmail.com>
2023-01-17 14:04:21 +1100

    updated react from deployed repo

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -4,7 +4,7 @@ import Spinner from './Spinner'
 export default function App() {
 
 	const [formData, setFormData] = useState({
-		prompt: 'bunny in the style of picasso',
+		prompt: '',
 		size: '512x512'
 	})
 
@@ -30,25 +30,28 @@ export default function App() {
 		try {
 
 			setShowSpinner(true)
-			console.log('showing spinner')
 			
-			fetch('/generateimage', {
+			fetch('https://openai-image-generator-flask-backend.onrender.com/generateimage', {
 				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json'
 				},
 				body: JSON.stringify(formData)
 			})
-			.then(res => res.json())
+			.then(response => {
+				if (!response.ok) {
+					setShowSpinner(false)
+					throw new Error("Image could not be generated")
+				}
+				return response.json()
+			})
 			.then(data => {
-				// console.log(data)
 				setData(data)
 				setShowSpinner(false)
-				console.log('hiding spinner')
 			})
 
 		} catch (error) {
-			
+			setErrorMessage(error)
 		}
 	}
 	
@@ -56,21 +59,21 @@ export default function App() {
 	return (
 		<div>
 			<main className='container mx-auto mt-12 max-w-2xl bg-white/40 rounded'>
-				<form className="p-6 space-y-4 rounded border-gray-800">
+				<form className="p-6 space-y-4 rounded border-gray-800 backdrop-blur-xl">
 					
 					<textarea
 						name="prompt"
-						placeholder='Type a prompt...'
-						className='rounded w-full text-gray-500 border-gray-400 font-bold placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-sky-500'
+						placeholder='Enter prompt...'
+						className='rounded w-full text-gray-600 border-gray-400 font-bold placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-sky-500 required'
 						onChange={handleChange}
 						value={formData.prompt}
 						></textarea>
 			
-					<div className='flex space-x-2'>
+					<div className='flex space-x-6'>
 
 						<select
 							name="size"
-							className='rounded border-gray-400 text-gray-500 focus:outline-none focus:ring-1 focus:ring-sky-500 '
+							className='rounded border-gray-400 text-gray-600 focus:outline-none focus:ring-1 focus:ring-sky-500 '
 							value={formData.size}
 							onChange={handleChange}
 							>
@@ -86,12 +89,17 @@ export default function App() {
 							className='w-full border border-gray-500 rounded p-2 px-4 font-bold text-white
 							border-none bg-green-600 hover:cursor-pointer hover:bg-green-500 active:bg-green-400 transition'/>
 					</div>
+					
 				</form>
-			
+
+
 				{data.image_url && <img src={data.image_url} alt="generated" className='w-full p-6 pt-0' />}
 			</main>
+			
 
 			{showSpinner && <Spinner />}
+
+			{errorMessage && <p className='bg-red-100 rounded-lg py-5 px-6 mb-4 text-base text-red-700 mb-3'>{errorMessage}</p>}
 			
 		</div>
 	)
